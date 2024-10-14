48b3dfb98d6fa48dcb2befee92759280d7060562
kelseaconrad19 <163567548+kelseaconrad19@users.noreply.github.com>
2024-10-14 10:46:14 -0400

    adjusting requirements and adding new_app file

---
+++ new_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+new_app = Flask(__name__)
+
+@new_app.route('/sum', methods=['POST'])
+def sum():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return jsonify({'result': result})
